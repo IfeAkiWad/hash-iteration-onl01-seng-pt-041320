0b49ee16b5e09c2439f9f7c0e4404945cad24d18
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-19 23:33:08 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,10 +9,4 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
-if age < 12
-  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-elsif age > 12
-  puts "Yo Happy birthday Amanda! You are now 27 years old!"
-else
-  puts "Whose birthday is it?"
-end
+
