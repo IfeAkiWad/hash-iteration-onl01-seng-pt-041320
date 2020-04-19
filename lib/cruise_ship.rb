054bb5b1346da17793d86798a641b25040e4cc87
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-19 22:36:31 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,6 +10,6 @@ def select_winner(passengers)
 winner = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
-    return winner = name
-  end # add the code snippet here!
+  winner = name
+  end 
 end
